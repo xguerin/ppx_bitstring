f0917858b068f70334709d1dc1533c48b7e59d93
Xavier Guérin <github@applepine.org>
2016-09-13 12:43:01 -0400

    Add test for save_offset_to()

--- tests/BitstringQualifierTest.ml
+++ tests/BitstringQualifierTest.ml
@@ -21,7 +21,7 @@ open Bitstring
  * Test of the map() qualifier
  *)
 
-let map_parse_test context =
+let map_test context =
   let source = [%bitstring {| 1 : 16 ; 2 : 16 |}] in
   match%bitstring source with
   | {| value0 : 16 : map (fun v -> v + 1)
@@ -35,11 +35,30 @@ let map_parse_test context =
   | {| _ |} -> assert_bool "Invalid pattern" false
 
 (*
+ * Test of the save_offset_to() qualifier
+ *)
+
+let save_offset_test context =
+  let source = [%bitstring {| 1 : 3 ; 2 : 7; 5 : 4; "abc" : -1 : string |}] in
+  match%bitstring source with
+  | {| _      :  3 : save_offset_to (off0)
+     ; _      :  7 : save_offset_to (off1)
+     ; _      :  4 : save_offset_to (off2)
+     ; "abc"  : 24 : save_offset_to (off3), string
+    |} ->
+    assert_equal off0 0;
+    assert_equal off1 3;
+    assert_equal off2 10;
+    assert_equal off3 14
+  | {| _ |} -> assert_bool "Invalid pattern" false
+
+(*
  * Test suite definition
  *)
 
 let suite = "BitstringQualifierTest" >::: [
-    "map_parse"   >:: map_parse_test
+    "map"             >:: map_test;
+    "save_offset_to"  >:: save_offset_test
   ]
 
 let () = run_test_tt_main suite
